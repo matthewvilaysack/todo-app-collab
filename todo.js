8f94637f99a91a1344a227a62dc30085c5c7f29c
Maria <mariawang90@gmail.com>
2022-08-02 00:48:26 -0400

    mar commit

---
+++ todo.js
@@ -0,0 +1,12 @@
+// Draws a filled rectangle below "Task List"
+var c = document.getElementById("line");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.rect(0, 0, 200, 5);
+ctx.fillStyle = "black";
+ctx.stroke();
+
+//Function to add a new task
+function addTaskFunction(form) {
+    
+}
